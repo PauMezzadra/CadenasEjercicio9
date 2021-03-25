8d4f5653b25fb1316c7072bcb886ec46c5379747
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 15:07:03 -0300

    Add project files.

---
+++ CadenasEjercicio9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadenasEjercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "";
+            do
+            {
+                Console.WriteLine("Ingrese un texto y yo se lo mostraré en mayúsculas");
+                texto = Console.ReadLine().ToUpper();
+                if (texto == "")
+                {
+                    Console.WriteLine("No puede ser vacío");
+                }
+                else
+                {
+                    Console.WriteLine($"Su frase es: {texto}");
+                }
+            } while (texto == "");
+
+            Console.ReadKey();
+        }
+    }
+}
